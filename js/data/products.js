2f1e1089d34716e07c0a4111b8d7878c1571b176
YanaEgorova <yanaegorova69.3@gmail.com>
2021-08-21 00:24:56 +0300

    +

---
+++ js/data/products.js
@@ -0,0 +1,314 @@
+export const products = [
+    {
+        id: 'product-1',
+        name: 'Silicone Protective Case Cover',
+        price: 5.95,
+        image: './img/img1.png',
+        description: [
+            {
+                text: 'Watch protective cover is made of high-quality soft and flexible silicone, which is safty and comfortable.',
+            },
+            {
+                text: '42mm silicone protective case can protect your  watch from water, dirt, scratch, and keep your watch in a good condition for a longer time.',
+            },
+            {
+                text: 'You can choose different colors to change your watch\'s color to give your different usage\'s experience in daily life.',
+            },
+            {
+                text: '42mm silicone case is easy to install and remove.',
+            },
+        ],
+        images: ['./img/img1-1.png', './img/img1-2.png'],
+    },
+    {
+        id: 'product-2',
+        name: 'Shale Step Ledge for Aquariums',
+        price: 19.99,
+        image: './img/img2.png',
+        description: [
+            {
+                title: 'Perfect for reptiles, small fish, and other aquatic pets',
+                text: 'Shale Cave Hide-Out is a great addition to any aquarium or terrarium. It’s perfect for territorial fish, small reptiles, newts, salamanders, crayfish, axolotls, snakes, and more.',
+            },
+            {
+                title: 'Realistic rock formation',
+                text: 'The beautiful hand painted details make this hideout a perfect natural-looking addition to your animal’s habitat - It will blend seamlessly with any environment, aquatic or dry.',
+            },
+            {
+                title: 'Best used for climbing and hiding spots',
+                text: 'No matter what animal you’ve got, they’ll love this Shale Cave. They’ll climb up, over, and around, and use it as a hiding space.',
+            },
+            {
+                title: 'Helps you mimic the look of a natural habitat',
+                text: 'The Shale Cave is designed to mimic natural rocks found in desert environments. It’ll look great in an aquarium, terrarium, and any other kind of habitat that’s home to a small animal.',
+            },
+        ],
+        images: ['./img/img2-1.png', './img/img2-2.png', './img/img2-3.png', './img/img2-4.png'],
+    },
+    {
+        id: 'product-3',
+        name: 'Plastic Gasoline Fuel Can',
+        price: 24.99,
+        discount: 14.99,
+        image: './img/img3.png',
+        description: [
+            {
+                text: '5 Gallon Plastic Gasoline Fuel Can, not compatible with greater than 10% ethanol fuel, Holds 5 gallons (20 liters) of gas and oil mixes, Exceeds California Air Resource Board (CARB) and EPA requirements for portable fuel containers, Made of durable HDPE with barrier materials to virtually eliminate hydrocarbon emissions, Built with a wider base and lower center of gravity to reduce the chance of a can tipping over.',
+            },
+        ],
+        images: ['./img/img3-1.png', './img/img3-2.png'],
+    },
+    {
+        id: 'product-4',
+        name: 'Palm Leaf Inflatable',
+        price: 35.32,
+        discount: 24.65,
+        image: './img/img4.png',
+        description: [
+            {
+                text: 'Package dimensions :3.0" L x10.5" W x12.0" H',
+            },
+            {
+                text: 'Package weight :4.7pounds',
+            },
+            {
+                text: 'Country of origin :China',
+            },
+        ],
+        images: ['./img/img4-1.png', './img/img4-2.png', './img/img4-3.png', './img/img4-4.png'],
+    },
+    {
+        id: 'product-5',
+        name: 'Universal stylus',
+        price: 39.99,
+        discount: 39.97,
+        image: './img/img5.png',
+        description: [
+            {
+                title: 'Dual Tip Stylus',
+                text: 'Navigate and scroll with the universal capacitive backend tip and write or draw with the active tip.',
+            },
+            {
+                title: 'Tilt Recognition',
+                text: 'Stylus has tilt recognition so you can vary the width of your stroke.',
+            },
+            {
+                title: 'Palm Rejection Technology',
+                text: 'Only the stylus will register on your tablet, even if your hand touches your screen.',
+            },
+            {
+                title: 'Increase your productivity',
+                text: 'Scroll and navigate pages with ease, or make notes, sketch, mark up documents, and more.',
+            },
+            {
+                text: 'The Stylus lasts up to nine hours before it needs to be recharged.',
+            },
+        ],
+        images: ['./img/img5-1.png', './img/img5-2.png'],
+    },
+    {
+        id: 'product-6',
+        name: 'Pouf, Natural',
+        price: 43.98,
+        discount: 55.97,
+        image: './img/img6.png',
+        description: [
+            {
+                text: 'Made in India.',
+            },
+            {
+                text: 'Real Jute.',
+            },
+            {
+                text: 'No assembly necessary.',
+            },
+            {
+                text: 'Small size is great for limited space.',
+            },
+        ],
+        images: ['./img/img6-1.png', './img/img6-2.png', './img/img6-3.jpg'],
+    },
+    {
+        id: 'product-7',
+        name: 'Airbrush',
+        price: 49.95,
+        discount: 69.99,
+        image: './img/img7.png',
+        description: [
+            {
+                text: 'This is a dual action gravity feed airbrush; comes with instruction manual.',
+            },
+            {
+                text: 'Innovative and efficient precision design is enjoyed by crafters, artists, and hobbyists.',
+            },
+            {
+                text: 'Single (.5) needle/nozzle for spraying all Mediums, inks, dyes, watercolors, acrylics, enamels, lacquers, glazes, latex and etc.',
+            },
+            {
+                text: 'Easy maintenance self- centering nozzle design; acute tapered color well for faster cleaning; Maximum angle trigger clearance and stroke; Patented easy access needle removal system.',
+            },
+        ],
+        images: ['./img/img7-1.png', './img/img7-2.png', './img/img7-3.png'],
+    },
+    {
+        id: 'product-8',
+        name: 'Floor Lamp',
+        price: 69.99,
+        discount: 69.94,
+        image: './img/img8.png',
+        description: [
+            {
+                text: 'Constructed of Metal.',
+            },
+            {
+                text: '3-Way Rotary Switch.',
+            },
+            {
+                text: 'Requires (1) 150 watt bulb, not included.',
+            },
+            {
+                text: 'Metal Ball Accent.',
+            },
+        ],
+        images: ['./img/img8-1.png', './img/img8-2.png', './img/img8-3.png', './img/img8-4.png'],
+    },
+    {
+        id: 'product-9',
+        name: 'Refractor Telescope',
+        price: 79.99,
+        discount: 79.93,
+        image: './img/img9.png',
+        description: [
+            {
+                text: 'Entry-level telescope with 300x power and 400mm focal length.',
+            },
+            {
+                text: 'Coated glass optics for enhanced image brightness.',
+            },
+            {
+                text: 'Interchangeable eyepieces with 3x lenses for versatility.',
+            },
+            {
+                text: 'Includes beginner\'s version of Deepsky Astronomy software.',
+            },
+            {
+                text: 'Adjustable tripod and soft carrying case.',
+            },
+        ],
+        images: ['./img/img9-1.png', './img/img9-2.png', './img/img9-3.png'],
+    },
+    {
+        id: 'product-10',
+        name: 'Student Biological Compound Microscope',
+        price: 84.41,
+        discount: 95.50,
+        image: './img/img10.png',
+        description: [
+            {
+                text: 'This is an ideal microscope for home school or for students in elementary to high school to learn sciences.',
+            },
+            {
+                text: '360 degree rotatable monocular head offers five magnification settings 40X, 100X, 250X, 400X & 1000X.',
+            },
+            {
+                text: 'Widefield all optical glass elements includes single lens condenser with disc diaphragm.',
+            },
+            {
+                text: 'Sturdy all metal framework. Power Supply - 110 V.',
+            },
+            {
+                text: 'LED illumination system uses either an outlet (adapter included) or three AA batteries (or re-chargeable batteries).',
+            },
+        ],
+        images: ['./img/img10-1.png', './img/img10-2.png', './img/img10-3.png', './img/img10-4.png'],
+    },
+    {
+        id: 'product-11',
+        name: 'Coffee Maker',
+        price: 89.99,
+        discount: 109.91,
+        image: './img/img11.png',
+        description: [
+            {
+                title: 'Fits anywhere',
+                text: 'Less than 5 inches wide, fits neatly on your countertop.',
+            },
+            {
+                title: 'Multiple cup water reservoir',
+                text: 'Removable 46 oz. reservoir lets you brew up to 4 cups before refilling.',
+            },
+            {
+                title: '3 cup sizes',
+                text: 'Brew an 8, 10, or 12 oz. cup at the push of a button.',
+            },
+            {
+                title: 'Fast & fresh brewed',
+                text: 'Delicious coffee made in minutes.',
+            },
+            {
+                title: 'Travel mug friendly',
+                text: 'Removable drip tray accommodates travel mugs up to 7.0” tall and holds a full accidental brew for easy cleanup.',
+            },
+        ],
+        images: ['./img/img11-1.png', './img/img11-2.png', './img/img11-3.png'],
+    },
+    {
+        id: 'product-12',
+        name: 'Trek Slim Sole Hoof Boot',
+        price: 109.00,
+        discount: 110.89,
+        image: './img/img12.png',
+        description: [
+            {
+                text: 'Simple to put on and remove, stays securely on hoof.',
+            },
+            {
+                text: 'Absorbs shock and concussion.',
+            },
+            {
+                text: 'Built-in drainage.',
+            },
+            {
+                text: 'Fits left or right hoof, available individually.',
+            },
+            {
+                text: 'Replaceable RES Velcro closure.',
+            },
+        ],
+        images: ['./img/img12-1.png', './img/img12-2.jpg'],
+    },
+    {
+        id: 'product-13',
+        name: 'Rolling Cooler',
+        price: 119.00,
+        discount: 120.88,
+        image: './img/img13.png',
+        description: [
+            {
+                title: 'Xtreme Technology',
+                text: 'Insulated lid and extra wall insulation keep ice up to 5 days in temperatures as high as 90°F.',
+            },
+            {
+                title: 'High capacity',
+                text: 'Holds up to 84 cans.',
+            },
+            {
+                title: 'Heavy-duty wheels',
+                text: 'Effortless rolling over any terrain.',
+            },
+            {
+                title: 'Have-a-Seat Lid',
+                text: 'Closed Lid Supports Up to 250 Lb.',
+            },
+            {
+                title: 'Cup holders',
+                text: 'Molded into the lid to keep drinks close and prevent spilling.',
+            },
+            {
+                title: 'Telescoping handle',
+                text: 'For easy pulling and compact storage.',
+            },
+        ],
+        images: ['./img/img13-1.png', './img/img13-2.png', './img/img13-3.png', './img/img13-4.png', './img/img13-5.png', './img/img13-6.png', './img/img13-7.png', './img/img13-8.png'],
+    },
+]

e9de8249edc51cdbee66e5472636bb19bf82e4d4
YanaEgorova <yanaegorova69.3@gmail.com>
2021-08-24 01:40:37 +0300

    +

--- js/data/products.js
+++ js/data/products.js
@@ -2,7 +2,7 @@ export const products = [
     {
         id: 'product-1',
         name: 'Silicone Protective Case Cover',
-        price: 5.95,
+        price: 6.96,
         image: './img/img1.png',
         description: [
             {
@@ -22,33 +22,8 @@ export const products = [
     },
     {
         id: 'product-2',
-        name: 'Shale Step Ledge for Aquariums',
-        price: 19.99,
-        image: './img/img2.png',
-        description: [
-            {
-                title: 'Perfect for reptiles, small fish, and other aquatic pets',
-                text: 'Shale Cave Hide-Out is a great addition to any aquarium or terrarium. It’s perfect for territorial fish, small reptiles, newts, salamanders, crayfish, axolotls, snakes, and more.',
-            },
-            {
-                title: 'Realistic rock formation',
-                text: 'The beautiful hand painted details make this hideout a perfect natural-looking addition to your animal’s habitat - It will blend seamlessly with any environment, aquatic or dry.',
-            },
-            {
-                title: 'Best used for climbing and hiding spots',
-                text: 'No matter what animal you’ve got, they’ll love this Shale Cave. They’ll climb up, over, and around, and use it as a hiding space.',
-            },
-            {
-                title: 'Helps you mimic the look of a natural habitat',
-                text: 'The Shale Cave is designed to mimic natural rocks found in desert environments. It’ll look great in an aquarium, terrarium, and any other kind of habitat that’s home to a small animal.',
-            },
-        ],
-        images: ['./img/img2-1.png', './img/img2-2.png', './img/img2-3.png', './img/img2-4.png'],
-    },
-    {
-        id: 'product-3',
         name: 'Plastic Gasoline Fuel Can',
-        price: 24.99,
+        price: 29.99,
         discount: 14.99,
         image: './img/img3.png',
         description: [
@@ -59,101 +34,9 @@ export const products = [
         images: ['./img/img3-1.png', './img/img3-2.png'],
     },
     {
-        id: 'product-4',
-        name: 'Palm Leaf Inflatable',
-        price: 35.32,
-        discount: 24.65,
-        image: './img/img4.png',
-        description: [
-            {
-                text: 'Package dimensions :3.0" L x10.5" W x12.0" H',
-            },
-            {
-                text: 'Package weight :4.7pounds',
-            },
-            {
-                text: 'Country of origin :China',
-            },
-        ],
-        images: ['./img/img4-1.png', './img/img4-2.png', './img/img4-3.png', './img/img4-4.png'],
-    },
-    {
-        id: 'product-5',
-        name: 'Universal stylus',
-        price: 39.99,
-        discount: 39.97,
-        image: './img/img5.png',
-        description: [
-            {
-                title: 'Dual Tip Stylus',
-                text: 'Navigate and scroll with the universal capacitive backend tip and write or draw with the active tip.',
-            },
-            {
-                title: 'Tilt Recognition',
-                text: 'Stylus has tilt recognition so you can vary the width of your stroke.',
-            },
-            {
-                title: 'Palm Rejection Technology',
-                text: 'Only the stylus will register on your tablet, even if your hand touches your screen.',
-            },
-            {
-                title: 'Increase your productivity',
-                text: 'Scroll and navigate pages with ease, or make notes, sketch, mark up documents, and more.',
-            },
-            {
-                text: 'The Stylus lasts up to nine hours before it needs to be recharged.',
-            },
-        ],
-        images: ['./img/img5-1.png', './img/img5-2.png'],
-    },
-    {
-        id: 'product-6',
-        name: 'Pouf, Natural',
-        price: 43.98,
-        discount: 55.97,
-        image: './img/img6.png',
-        description: [
-            {
-                text: 'Made in India.',
-            },
-            {
-                text: 'Real Jute.',
-            },
-            {
-                text: 'No assembly necessary.',
-            },
-            {
-                text: 'Small size is great for limited space.',
-            },
-        ],
-        images: ['./img/img6-1.png', './img/img6-2.png', './img/img6-3.jpg'],
-    },
-    {
-        id: 'product-7',
-        name: 'Airbrush',
-        price: 49.95,
-        discount: 69.99,
-        image: './img/img7.png',
-        description: [
-            {
-                text: 'This is a dual action gravity feed airbrush; comes with instruction manual.',
-            },
-            {
-                text: 'Innovative and efficient precision design is enjoyed by crafters, artists, and hobbyists.',
-            },
-            {
-                text: 'Single (.5) needle/nozzle for spraying all Mediums, inks, dyes, watercolors, acrylics, enamels, lacquers, glazes, latex and etc.',
-            },
-            {
-                text: 'Easy maintenance self- centering nozzle design; acute tapered color well for faster cleaning; Maximum angle trigger clearance and stroke; Patented easy access needle removal system.',
-            },
-        ],
-        images: ['./img/img7-1.png', './img/img7-2.png', './img/img7-3.png'],
-    },
-    {
-        id: 'product-8',
+        id: 'product-3',
         name: 'Floor Lamp',
-        price: 69.99,
+        price: 64.90,
         discount: 69.94,
         image: './img/img8.png',
         description: [
@@ -173,57 +56,7 @@ export const products = [
         images: ['./img/img8-1.png', './img/img8-2.png', './img/img8-3.png', './img/img8-4.png'],
     },
     {
-        id: 'product-9',
-        name: 'Refractor Telescope',
-        price: 79.99,
-        discount: 79.93,
-        image: './img/img9.png',
-        description: [
-            {
-                text: 'Entry-level telescope with 300x power and 400mm focal length.',
-            },
-            {
-                text: 'Coated glass optics for enhanced image brightness.',
-            },
-            {
-                text: 'Interchangeable eyepieces with 3x lenses for versatility.',
-            },
-            {
-                text: 'Includes beginner\'s version of Deepsky Astronomy software.',
-            },
-            {
-                text: 'Adjustable tripod and soft carrying case.',
-            },
-        ],
-        images: ['./img/img9-1.png', './img/img9-2.png', './img/img9-3.png'],
-    },
-    {
-        id: 'product-10',
-        name: 'Student Biological Compound Microscope',
-        price: 84.41,
-        discount: 95.50,
-        image: './img/img10.png',
-        description: [
-            {
-                text: 'This is an ideal microscope for home school or for students in elementary to high school to learn sciences.',
-            },
-            {
-                text: '360 degree rotatable monocular head offers five magnification settings 40X, 100X, 250X, 400X & 1000X.',
-            },
-            {
-                text: 'Widefield all optical glass elements includes single lens condenser with disc diaphragm.',
-            },
-            {
-                text: 'Sturdy all metal framework. Power Supply - 110 V.',
-            },
-            {
-                text: 'LED illumination system uses either an outlet (adapter included) or three AA batteries (or re-chargeable batteries).',
-            },
-        ],
-        images: ['./img/img10-1.png', './img/img10-2.png', './img/img10-3.png', './img/img10-4.png'],
-    },
-    {
-        id: 'product-11',
+        id: 'product-4',
         name: 'Coffee Maker',
         price: 89.99,
         discount: 109.91,
@@ -253,7 +86,7 @@ export const products = [
         images: ['./img/img11-1.png', './img/img11-2.png', './img/img11-3.png'],
     },
     {
-        id: 'product-12',
+        id: 'product-5',
         name: 'Trek Slim Sole Hoof Boot',
         price: 109.00,
         discount: 110.89,
@@ -278,7 +111,7 @@ export const products = [
         images: ['./img/img12-1.png', './img/img12-2.jpg'],
     },
     {
-        id: 'product-13',
+        id: 'product-6',
         name: 'Rolling Cooler',
         price: 119.00,
         discount: 120.88,
